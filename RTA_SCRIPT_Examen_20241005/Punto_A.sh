da590f96fb6b3fdef5ee6249ccdf6eabff900687
Demian <nazkbro@gmail.com>
2024-10-06 21:06:03 -0400

    Realizaciòn del PUNTO A Y B.

---
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Creamos en la carpeta raiz los archivos con los alumnos, haciendo un tree vemos la lista.
+sudo mkdir -p /Examenes-UTN/{alumno1/parcial{1..3},alumno2/parcial{1..3},alumno3/parcial{1..3},profesores}
