2fc6a1982df28d3e9cf14662c3ec61fe225b278d
Demian <nazkbro@gmail.com>
2024-10-06 22:03:13 -0400

    Realizaciòn Punto C

--- RTA_SCRIPT_Examen_20241005/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Creamos en la carpeta raiz los archivos con los alumnos, haciendo un tree vemos la lista.
-sudo mkdir -p /Examenes-UTN/{alumno1/parcial{1..3},alumno2/parcial{1..3},alumno3/parcial{1..3},profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_1/parcial{1..3},alumno_2/parcial{1..3},alumno_3/parcial{1..3},profesores}
